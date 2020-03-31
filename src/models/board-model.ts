517ef5424773cd716ee91101b118456458dff854
Tomasz Wiech <tomasz.wiech@capgemini.com>
2020-03-31 16:12:44 +0200

    Fix empty icons in build.

--- src/models/board-model.ts
+++ src/models/board-model.ts
@@ -1,5 +1,17 @@
 import { List } from 'immutable'
 import { Field } from './field-model'
+import BlackPawn from '../../public/images/Chess_pdt45.svg'
+import WhitePawn from '../../public/images/Chess_plt45.svg'
+import WhiteRook from  '../../public/images/Chess_rlt45.svg'
+import WhiteKnight from  '../../public/images/Chess_nlt45.svg'
+import WhiteBishop from '../../public/images/Chess_blt45.svg'
+import WhiteQueen from  '../../public/images/Chess_qlt45.svg'
+import WhiteKing from  '../../public/images/Chess_klt45.svg'
+import BlackRook from  '../../public/images/Chess_rdt45.svg'
+import BlackKnight from  '../../public/images/Chess_ndt45.svg'
+import BlackBishop from  '../../public/images/Chess_bdt45.svg'
+import BlackQueen from  '../../public/images/Chess_qdt45.svg'
+import BlackKing from  '../../public/images/Chess_kdt45.svg'
 
 type Row = List<Field>
 
@@ -23,25 +35,25 @@ const types = List([
 ])
 
 const whiteIcons = List([
-  'icons/Chess_rlt45.svg',
-  'icons/Chess_nlt45.svg',
-  'icons/Chess_blt45.svg',
-  'icons/Chess_qlt45.svg',
-  'icons/Chess_klt45.svg',
-  'icons/Chess_blt45.svg',
-  'icons/Chess_nlt45.svg',
-  'icons/Chess_rlt45.svg'
+  WhiteRook,
+  WhiteKnight,
+  WhiteBishop,
+  WhiteQueen,
+  WhiteKing,
+  WhiteBishop,
+  WhiteKnight,
+  WhiteRook
 ])
 
 const blackIcons = List([
-  'icons/Chess_rdt45.svg',
-  'icons/Chess_ndt45.svg',
-  'icons/Chess_bdt45.svg',
-  'icons/Chess_qdt45.svg',
-  'icons/Chess_kdt45.svg',
-  'icons/Chess_bdt45.svg',
-  'icons/Chess_ndt45.svg',
-  'icons/Chess_rdt45.svg'
+  BlackRook,
+  BlackKnight,
+  BlackBishop,
+  BlackQueen,
+  BlackKing,
+  BlackBishop,
+  BlackKnight,
+  BlackRook
 ])
 
 const whiteFigures = types.zip(whiteIcons)
@@ -54,7 +66,7 @@ const blackFiguresRow = letters.zip(blackFigures).map(([l, [t, s]]) => {
 const blackPawnRow = letters.map(l => {
   return {
     coordinates: `${l}7`,
-    figure: { type: 'Pawn', icon: 'icons/Chess_pdt45.svg', color: 'Black' }
+    figure: { type: 'Pawn', icon: BlackPawn, color: 'Black' }
   }
 }) as Row
 
@@ -70,7 +82,7 @@ const emptyRows = numbers.map(n => {
 const whitePawnRow = letters.map(l => {
   return {
     coordinates: `${l}2`,
-    figure: { type: 'Pawn', icon: 'icons/Chess_plt45.svg', color: 'White' }
+    figure: { type: 'Pawn', icon: WhitePawn, color: 'White' }
   }
 }) as Row
 
