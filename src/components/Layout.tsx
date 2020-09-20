f93806a8eee3e4153116af5b36ddb061bd36ffa3
zsamotw <donkemot@interia.pl>
2020-09-20 16:03:44 +0200

    Move dndProvider to index.tsx.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,36 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 import Header from './Header'
 import Board from './Board'
-import { DndProvider } from 'react-dnd'
-import MultiBackend from 'react-dnd-multi-backend';
-import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch'; // or any other pipeline
 import GamePanel from './GamePanel'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
- `
+`
 
 const ContentWrapper = styled.div`
   display: flex;
   margin: 3rem auto;
 
-  @media screen and (max-width: 1024px){
+  @media screen and (max-width: 1024px) {
     flex-direction: column;
   }
 `
 
 export default function Layout() {
-
   return (
-    <DndProvider backend={ MultiBackend } options={HTML5toTouch}>
-      <Container>
-          <Header></Header>
-        <ContentWrapper>
-          <Board></Board>
-          <GamePanel></GamePanel>
-        </ContentWrapper>
-      </Container>
-    </DndProvider>
+    <Container>
+      <Header></Header>
+      <ContentWrapper>
+        <Board></Board>
+        <GamePanel></GamePanel>
+      </ContentWrapper>
+    </Container>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,17 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import { DndProvider } from 'react-dnd'
+import MultiBackend from 'react-dnd-multi-backend'
+import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch' // or any other pipeline
 
 ReactDOM.render(
   <Provider store={store['store']}>
-    <App />
+    <DndProvider backend={MultiBackend} options={HTML5toTouch}>
+      <App />
+    </DndProvider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 )
 
 // If you want your app to work offline and load faster, you can change
