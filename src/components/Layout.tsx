18dc45e74d3065b5920f386ab04f5730ee4eee3e
zsamotw <donkemot@interia.pl>
2020-10-06 18:38:51 +0200

    Add footer.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import Header from './Header'
+import Footer from './Footer'
 import Board from './Board'
 import GamePanel from './GamePanel'
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  min-height: 100vh;
 `
 
 const ContentWrapper = styled.div`
   display: flex;
   margin: 3rem auto;
+  flex: 1 1 auto;
 
   @media screen and (max-width: 1024px) {
     flex-direction: column;
@@ -26,6 +29,7 @@ export default function Layout() {
         <Board></Board>
         <GamePanel></GamePanel>
       </ContentWrapper>
+      <Footer></Footer>
     </Container>
   )
 }
